1f8e7b8554a522a34ea27cb885c9375bc1c1b118
Rafael Ferreira <rafaelnevesf@gmail.com>
2022-01-13 16:04:18 +0100

    Registration form

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,9 +1,52 @@
 import { Component } from "react";
+import {Form, Button} from 'react-bootstrap';
 
 class Registration extends Component {
+
+    state = {
+        name: '',
+        surname: '',
+        email: '',
+        password: '',
+        passwordConfirm: '',
+        completedForm: false
+    }
+
     render() {
-        return(
-            <h1 className="text-white">Form</h1>
+        return (
+            <>
+                <h1 className="text-white">FORM</h1>
+                <Form>
+                    <Form.Group>
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter name" />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label>Surname</Form.Label>
+                        <Form.Control type="text" placeholder="Enter surname" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label>Confirm password</Form.Label>
+                        <Form.Control type="password" placeholder="Confirm password" />
+                    </Form.Group>
+                    
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </>
         )
     }
 }
