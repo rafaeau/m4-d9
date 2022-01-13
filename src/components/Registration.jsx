1b5d7104d631faa1544d052c367ab51bf0913cf4
Rafael Ferreira <rafaelnevesf@gmail.com>
2022-01-13 16:41:07 +0100

    Form requirements working

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -23,6 +23,18 @@ class Registration extends Component {
         })
     }
 
+    handleButton = () => {
+        let clickableButton = false
+        if(this.state.userData.name.length >= 2 &&
+            this.state.userData.surname.length >= 3 &&
+            this.state.userData.password.length >= 8 &&
+            this.state.userData.password.match(/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/) &&
+            this.state.userData.passwordConfirm === this.state.userData.password
+            ){
+            clickableButton = true
+        } return clickableButton
+    }
+
     render() {
         return (
             <>
@@ -30,30 +42,30 @@ class Registration extends Component {
                 <Form>
                     <Form.Group>
                         <Form.Label>Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter name" onChange={(e) => this.handleInput('name', e.target.value)} value={this.state.name}/>
+                        <Form.Control required type="text" placeholder="Enter name" onChange={(e) => this.handleInput('name', e.target.value)} value={this.state.name}/>
                     </Form.Group>
 
                     <Form.Group>
                         <Form.Label>Surname</Form.Label>
-                        <Form.Control type="text" placeholder="Enter surname" onChange={(e) => this.handleInput('surname', e.target.value)} value={this.state.surname}/>
+                        <Form.Control required type="text" placeholder="Enter surname" onChange={(e) => this.handleInput('surname', e.target.value)} value={this.state.surname}/>
                     </Form.Group>
 
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" onChange={(e) => this.handleInput('email', e.target.value)} value={this.state.email}/>
+                        <Form.Control required type="email" placeholder="Enter email" onChange={(e) => this.handleInput('email', e.target.value)} value={this.state.email}/>
                     </Form.Group>
 
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" onChange={(e) => this.handleInput('password', e.target.value)} value={this.state.password}/>
+                        <Form.Control required type="password" placeholder="Password" onChange={(e) => this.handleInput('password', e.target.value)} value={this.state.password}/>
                     </Form.Group>
 
                     <Form.Group>
                         <Form.Label>Confirm password</Form.Label>
-                        <Form.Control type="password" placeholder="Confirm password" onChange={(e) => this.handleInput('passwordConfirm', e.target.value)} value={this.state.passwordConfirm}/>
+                        <Form.Control required type="password" placeholder="Confirm password" onChange={(e) => this.handleInput('passwordConfirm', e.target.value)} value={this.state.passwordConfirm}/>
                     </Form.Group>
                     
-                    <Button variant="success" type="submit">
+                    <Button variant="success" type="submit" disabled={!this.handleButton()}>
                         Submit
                     </Button>
                 </Form>
